f4c672589db019d29d4e3b23c6aadc78331281b9
Samuel Saputra <samuel.saputra@modalrakyat.id>
2022-03-05 14:19:26 +0700

    fix code and folder structure

--- src/Components/Ballot/index.js
+++ src/Components/Ballot/index.js
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const Ballot = () => {
   return (
     <div className='ballot'>
---
+++ src/Containers/App/index.js
@@ -0,0 +1,9 @@
+import Ballot from '../../Components/Ballot';
+
+const App = () => (
+  <div className="App">
+    <Ballot />
+  </div>
+);
+
+export default App;
